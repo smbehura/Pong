f46cdb8b152f27369391931909bfcdf89e9755d4
cokumko <cokumko@mit.edu>
2016-01-19 17:57:49 -0500

    Update ball_class.py
    added set_color function
    added self parameter

--- ball_class.py
+++ ball_class.py
@@ -7,16 +7,19 @@ from physical_object import PhysObj
 import pygame
 
 class Ball(PhysObj):
-	def __init__(color, vel):
+	def __init__(self, color, vel):
 		self.color = color
 		self.velocity = vel
 		pass
 
-	def set_vel(velocity):
+	def set_vel(self, velocity):
 		self.velocity = velocity
+		
+	def set_color(self, color):
+		self.color = color
 
-	def get_color():
+	def get_color(self):
 		return self.color
 
-	def get_velocity():
+	def get_velocity(self):
 		return self.velocity
