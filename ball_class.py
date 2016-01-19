a37ddbdc00f450b8b773c0fbea77c61d10ab4f85
smbehura <smbehura@gmail.com>
2016-01-18 23:46:08 -0500

    Create ball_class.py

---
+++ ball_class.py
@@ -0,0 +1,18 @@
+'''
+Ball Class
+'''
+
+class Ball(object):
+	def __init__(color, vel):
+		self.color = color
+		self.velocity = vel
+		pass
+
+	def set_vel(velocity):
+		self.velocity = velocity
+
+	def get_color():
+		return self.color
+
+	def get_velocity():
+		return self.velocity
