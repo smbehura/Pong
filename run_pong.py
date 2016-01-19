27ec3362270eb4889933aa9df3786dd78782afdc
smbehura <smbehura@gmail.com>
2016-01-19 14:52:24 -0500

    Update run_pong.py

--- run_pong.py
+++ run_pong.py
@@ -2,4 +2,18 @@
 Used to run and test Pong
 '''
 
+import pygame
+
+from board_class import Board
+
+pygame.init()
+screen = pygame.display.set_mode((600,250)) #subject to change
+pygame.display.set_caption('Pong')
+
+game = Board() #something in here
+
+game.play()
+
+
+
 # initialize screen, do stuff idk
