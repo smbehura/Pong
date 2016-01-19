a56492fc66712690207747735f6f2305666517f2
smbehura <smbehura@gmail.com>
2016-01-19 08:39:47 -0500

    Create paddle_class.py

---
+++ paddle_class.py
@@ -0,0 +1,27 @@
+'''
+Paddle Class
+'''
+import pygame
+
+class Paddle(object):
+	def __init__(color, location):
+		self.color = color
+		self.location = location #based on orientation
+		#need funciton in player class to turn player orientation into paddle location
+
+	def get_color():
+		return self.color
+
+	def get_loc():
+		return self.location
+
+	def move():
+		pass
+
+	def isPaddleofPlayer(player):
+		# this function will probably need to moved to the player class
+		pass
+
+	def draw():
+		# draw paddle using location and stuff
+		pass
