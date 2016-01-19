a360ca8a27d94de6bc0d677d35d15d49218c21d5
smbehura <smbehura@gmail.com>
2016-01-19 10:38:37 -0500

    Create physical_object.py

---
+++ physical_object.py
@@ -0,0 +1,13 @@
+'''
+Physical Object class specifically to handle collisions and such things
+'''
+
+import pygame
+
+class PhysObj(object):
+  def __init__(location):
+    self.location = location
+  
+  def isCollis(obj):
+    # checks if input obj is touching self
+    pass
