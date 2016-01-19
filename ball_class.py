dda8bc9ff04ed92683c744b7e5ea9e74013a6d27
cokumko <cokumko@mit.edu>
2016-01-19 17:59:36 -0500

    Update ball_class.py
    added position variables

--- ball_class.py
+++ ball_class.py
@@ -7,9 +7,11 @@ from physical_object import PhysObj
 import pygame
 
 class Ball(PhysObj):
-	def __init__(self, color, vel):
+	def __init__(self, color, vel, x, y):
 		self.color = color
 		self.velocity = vel
+		self.pos_x = x
+		self.pos_y = y
 		pass
 
 	def set_vel(self, velocity):
@@ -23,3 +25,9 @@ class Ball(PhysObj):
 
 	def get_velocity(self):
 		return self.velocity
+	
+	def get_x(self):
+		return self.pos_x
+		
+	def get_y(self):
+		return self.pos_y
