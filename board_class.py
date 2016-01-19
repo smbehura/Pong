d7a4af564d604f5367f2f7567a4264786d11916f
cokumko <cokumko@mit.edu>
2016-01-19 18:07:49 -0500

    Update board_class.py
    added self parameters
    added lists for walls and paddles

--- board_class.py
+++ board_class.py
@@ -5,23 +5,25 @@ Board Class
 import pygame
 
 class Board(object):
-	def __init__(numPlayers, numBalls):
+	def __init__(self, numPlayers, numBalls):
 		self.numPlayers = numPlayers
 		self.state = 0
 		self.numBalls = numBalls
 		self.balls = [] # list of balls in game which depends on self.numBalls
 		self.players = [] # list of players in game which depends on self.numPlayers
+		self.paddles = [] #list of paddles in game which depends on self.numPlayers
+		self.walls = [] #list of the positions of the walls that will be used for bouncing
 
-	def setNumPlay(num):
+	def setNumPlay(self, num):
 		self.numPlayers = num
 
-	def changeToLoss(player):
+	def changeToLoss(self, player):
 		# sets specified player's state variable to loss
 		pass
 
-	def checkForWin():
+	def checkForWin(self):
 		# loops through all the players and checks for win
 		pass
 	
-	def play():
+	def play(self):
 		# runs through entire game and plays Pong, must include ball starting etc.
