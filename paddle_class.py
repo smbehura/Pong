a5a37c94a9266ffb8aebb2aa71a85a6a6002ac61
cokumko <cokumko@mit.edu>
2016-01-19 18:04:45 -0500

    Update paddle_class.py
    added self parameters
    added x and y variable for drawing
    not sure that we need the draw function considering pygame already implements it (*)

--- paddle_class.py
+++ paddle_class.py
@@ -7,24 +7,34 @@ from physical_object import PhysObj
 import pygame
 
 class Paddle(PhysObj):
-	def __init__(color, location):
+	def __init__(self, color, location, x, y):
 		self.color = color
 		self.location = location #based on orientation
 		#need funciton in player class to turn player orientation into paddle location
+		self.pos_x = x
+		self.pos_y = y
 
-	def get_color():
+	def get_color(self):
 		return self.color
 
-	def get_loc():
+	def get_loc(self):
 		return self.location
 
-	def move():
+	def move(self):
 		pass
 
-	def isPaddleofPlayer(player):
+	def isPaddleofPlayer(self, player):
 		# this function will probably need to moved to the player class
 		pass
 
-	def draw():
+	"""
+	def draw(self):
 		# draw paddle using location and stuff
 		pass
+	"""
+	
+	def get_x(self):
+		return self.pos_x
+		
+	def get_y(self):
+		return self.pos_y
