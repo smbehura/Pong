3359108d3b088628509dcd233710720ea16f4cf8
pshin66 <pshin66@hotmail.com>
2016-01-20 14:54:28 -0500

    Update ball_class.py

--- ball_class.py
+++ ball_class.py
@@ -7,21 +7,28 @@ from physical_object import PhysObj
 import pygame
 
 class Ball(PhysObj):
-	def __init__(self, color, vel, x, y):
+	def __init__(self, color, vel, x, y, angle):
 		self.image = pygame.image.load("blank.png").convert_alpha()
   		self.rect = self.image.get_rect()
 		self.color = color
 		self.velocity = vel
 		self.pos_x = x
 		self.pos_y = y
+		self.angle = angle
 		self.update_rect()
 		pass
 
 	def set_vel(self, velocity):
 		self.velocity = velocity
 
+	def set_angle(self,angle):
+		self.angle = angle
+
 	def set_color(self, color):
 		self.color = color
+
+	def get_angle(self):
+		return self.angle
 
 	def get_color(self):
 		return self.color
@@ -32,7 +39,7 @@ class Ball(PhysObj):
 	def is_collided(self):
 		#should return if it collides with a paddle, wall, or ball
 		#if position of ball is equal to pos range of paddles then return true
-		#loop through each paddle 
+		#loop through each paddle and add/subtract length of paddle
 
 	def changeColor(self):
 		#changes color based on paddle hit
