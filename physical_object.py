e1af17c6e2e10b4f40e229af80d30dd7dd334cc2
smbehura <smbehura@gmail.com>
2016-01-20 14:24:05 -0500

    Update physical_object.py

--- physical_object.py
+++ physical_object.py
@@ -13,15 +13,20 @@ class PhysObj(object):
 	self.pos_y = y
 	self.update_rect()
     
-  def isCollis(obj):
-    # checks if input obj is touching self
-    pass
+  def isCollis(self, obj):
+    x = obj.get_x
+    y = obj.get_y
+    rect = obj.get_rect
+    return self.rect.pygame.Rect.colliderect(rect)
   
   def get_x(self):
   	return self.pos_x
 
   def get_y(self):
 	return self.pos_y
+	
+  def get_rect(self):
+  	return self.rect
 	
   def update_rect(self):
         self.rect.x = pos_x
