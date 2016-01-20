55644045c87bc02ec7365234ca4d76eb7c48ae58
smbehura <smbehura@gmail.com>
2016-01-20 14:42:15 -0500

    Update wall_class.py

--- wall_class.py
+++ wall_class.py
@@ -7,3 +7,9 @@ import pygame
 class Wall(PhysOjb):
   def __init__(orientation):
     self.orientation = orientation
+    if orientation % 2 == 0:
+      self.image = pygame.image.load('wall_H.png').convert_alpha()
+    elif orientation % 2 == 1:
+      self.image = pygame.image.load('wall_V.png').convert_alpha()
+    else:
+      print 'Error: orientation is not defined or is not a number'
