8495eb460896d7cbb1ff070679b8d51ef289b441
cokumko <cokumko@mit.edu>
2016-01-20 05:06:14 -0500

    Update physical_object.py

--- physical_object.py
+++ physical_object.py
@@ -6,20 +6,24 @@ import pygame
 
 class PhysObj(object):
   def __init__(location, x, y):
-    self.location = location
-    self.pos_x = x
-    self.pos_y = y
+  	self.image = pygame.image.load("blank.png").convert_alpha()
+  	self.rect = self.image.get_rect()
+	self.location = location
+	self.pos_x = x
+	self.pos_y = y
+	self.update_rect()
     
   def isCollis(obj):
     # checks if input obj is touching self
     pass
   
   def get_x(self):
-		return self.pos_x
-		
-	def get_y(self):
-		return self.pos_y
-		
-	def update_rect():
-	  self.rect.x = pos_x
-	  self.rect.y = pos_y
+  	return self.pos_x
+
+  def get_y(self):
+	return self.pos_y
+	
+  def update_rect(self):
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+        
