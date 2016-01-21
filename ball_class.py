eddd8e958a16cfef6e3c98cb3e0b88a5e9742ef7
smbehura <smbehura@gmail.com>
2016-01-21 12:15:38 -0500

    Update ball_class.py

--- ball_class.py
+++ ball_class.py
@@ -37,22 +37,6 @@ class Ball(PhysObj):
     def get_velocity(self):
         return self.velocity
 
-    def is_collided(self):
-		if self.isCollis(#right paddle or left paddle):
-			ang = 180-self.angle
-		elif self.isCollis(#top or bottom paddle):
-			ang = 360-self.angle
-		if ang >= 360:
-			ang = ang-360
-		elif ang <=0:
-			ang = ang+360
-		self.set_angle(ang)
-		return self.get_angle()
-        #returns new angle of ball
-        #if position of ball is equal to pos range of paddles then return true
-        #loop through each paddle and add/subtract length of paddle
-        pass
-
     def changeColor(self):
         #changes color based on paddle hit
         pass
@@ -63,4 +47,26 @@ class Ball(PhysObj):
         self.update_rect()
 
     def changeDir(self, obj):
-        pass
+        if self.isCollis(obj):
+        	if type(obj) is Paddle:
+        		if obj.orientation % 2 == 0: # even number: top or bottom
+        			# bottom: 5*pi/4 --> 3*pi/4; 7*pi/4 --> pi/4
+        			# top: 3*pi/4 --> 5*pi/4; pi/4 --> 7*pi/4
+        			self.angle = 2 * math.pi - self.angle	
+        		elif obj.orientation % 2 == 1: # odd number: left or right
+        			# right: 7*pi/4 --> 5*pi/4; pi/4 --> 3*pi/4
+        			# left: 5*pi/4 --> 7*pi/4; 3*pi/4 --> pi/4
+        			if self.angle <= math.pi:
+        				self.angle = math.pi - self.angle
+        			elif self.angle > math.pi:
+        				self.angle = 3 * math.pi - self.angle
+        	elif type(obj) is Ball:
+        		# pi/4 --> 3*pi/4; 3*pi/4 --> pi/4; 5*pi/4 --> 7*pi/4; 7*pi/4 --> 5*pi/4
+        		if self.angle <= math.pi:
+        			self.angle = math.pi - self.angle
+       			elif self.angle > math.pi:
+       				self.angle = 3 * math.pi - self.angle
+	return self.get_angle()
+        #returns new angle of ball
+        #if position of ball is equal to pos range of paddles then return true
+        #loop through each paddle and add/subtract length of paddle
