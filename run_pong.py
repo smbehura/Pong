04e40f825623a2a52e0e2f7f4674a51932fb3515
cokumko <cokumko@mit.edu>
2016-01-21 14:11:36 -0500

    Update run_pong.py

--- run_pong.py
+++ run_pong.py
@@ -14,13 +14,21 @@ screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption('Pong')
 
 class Game(object):
-    def __init__(self, screen, board):
+    def __init__(self, screen, board, clock):
         self.screen = screen
         self.board = board
+        self.clock = clock
+        self.pause = False
 
-    def main_loop(self, num_players, num_balls, ball_speed, clock):
+    def pause(self):
+        if self.pause:
+            self.pause = False
+        else:
+            self.pause = True
+        
+    def main_loop(self, num_players, num_balls, ball_speed):
         quit = False
-        while not self.board.checkForWin() and not quit:
+        while not self.board.checkForWin() or not quit:
             self.screen.fill((0, 0, 0))
             self.board.paddles.draw(screen)
             self.board.balls.draw(screen)
@@ -28,46 +36,55 @@ class Game(object):
             pygame.display.flip()
 
             ###USER INPUT
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    quit = True
-                    break
-                if event.type == pygame.KEYDOWN:
-                    try:
-                        if event.key == pygame.K_COMMA:
-                            board.paddle[3].move(False)
-                        elif event.key == pygame.K_PERIOD:
-                            board.paddle[3].move(True)
-                        elif event.key == pygame.K_c:
-                            board.paddle[1].move(False)
-                        elif event.key == pygame.K_v:
-                            board.paddle[1].move(True)
-                        elif event.key == pygame.K_q:
-                            board.paddle[2].move(False)
-                        elif event.key == pygame.K_a:
-                            board.paddle[2].move(True)
-                        elif event.key == pygame.K_UP:
-                            board.paddle[0].move(False)
-                        elif event.key == pygame.K_DOWN:
-                            board.paddle[0].move(True)
-                    except Exception:
-                        print "Invalid move."
-
-            for ball in self.board.ball:
-                ball.move()
-            # look through all objects and check for collisions
-            # collision checking includes changeDir() from Ball Class
-            # check if ball is off screen and return orientation if ball is off screen with ball.offScreenOrientation()
-            #       offScreenOrientation() returns 0 if ball is still on screen so check for that to check for and set losers
-            # depending on whether there's a loser or not, change numplayers of game and start the new game immediately
+            if not self.pause:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        quit = True
+                        break
+                    if event.type == pygame.KEYDOWN:
+                        try:
+                            if event.key == pygame.K_COMMA:
+                                board.paddle[3].move(False)
+                            elif event.key == pygame.K_PERIOD:
+                                board.paddle[3].move(True)
+                            elif event.key == pygame.K_c:
+                                board.paddle[1].move(False)
+                            elif event.key == pygame.K_v:
+                                board.paddle[1].move(True)
+                            elif event.key == pygame.K_q:
+                                board.paddle[2].move(False)
+                            elif event.key == pygame.K_a:
+                                board.paddle[2].move(True)
+                            elif event.key == pygame.K_UP:
+                                board.paddle[0].move(False)
+                            elif event.key == pygame.K_DOWN:
+                                board.paddle[0].move(True)
+                            elif event.key == pygame.K_p:
+                                self.pause()
+                        except Exception:
+                            print "Invalid move."
+    
+                for ball in self.board.ball:
+                    ball.move()
+                    for object in self.board.objects:
+                        ball.changeDir(object)
+                    
+                    orientation = ball.offScreenOrientation()
+                    if orientation != 0:
+                        self.board.changeToLoss(orientation)
+                
+                self.screen.fill((0, 0, 0))
+                self.board.paddles.draw(screen)
+                self.board.balls.draw(screen)
+                self.board.walls.draw(screen)
+    
+                pygame.display.flip() # update screen
+            self.clock.tick(1)
             
-            self.screen.fill((0, 0, 0))
-            self.board.paddles.draw(screen)
-            self.board.balls.draw(screen)
-            self.board.walls.draw(screen)
-
-            pygame.display.flip() # update screen
-            clock.tick(1)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_p:
+                        self.pause()
         pygame.quit()
 
 result = -1
@@ -82,8 +99,8 @@ while result == -1:
         ball_speed = int(raw_input("Ball Speed (1 to 10): "))
         board = Board(num_players, num_balls, ball_speed)
         clock = pygame.time.Clock()
-        game = Game(screen, board)
-        game.main_loop(num_players, num_balls, ball_speed, clock)
+        game = Game(screen, board, clock)
+        game.main_loop(num_players, num_balls, ball_speed)
     if result == 2: #instructions
         screen.fill((0, 0, 0))
         pygame.display.flip()
