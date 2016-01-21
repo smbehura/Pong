2502b7aa17eb8629d483a06e78cced81e3f06a88
cokumko <cokumko@mit.edu>
2016-01-21 18:25:42 -0500

    Update run_pong.py

--- run_pong.py
+++ run_pong.py
@@ -5,6 +5,7 @@ Used to run and test Pong
 import pygame
 from example_menu import main as menu
 from example_menu import alt_main as instructions
+from example_menu import nalt_main as options
 
 from board_class import Board
 
@@ -95,60 +96,28 @@ class Game(object):
         pygame.quit()
 
 result = -1
+list_result = []
 while result == -1:
     screen.fill((0, 0, 0))
     pygame.display.flip()
     result = menu(screen)
-    if result == 1: #start game
-        print "***START GAME***"
-        num_players = int(raw_input("Number of Players (1 to 4): "))
-        num_balls = int(raw_input("Number of Balls (1 to 3): "))
-        ball_speed = int(raw_input("Ball Speed (1 to 10): "))
-        ball_speed /= 2
-        board = Board(num_players, num_balls, ball_speed)
-        clock = pygame.time.Clock()
-        game = Game(screen, board, clock)
-        game.main_loop(num_players, num_balls, ball_speed)
+    if result == 1: #get user values
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        result = 0
+        list_result = options(screen)
     if result == 2: #instructions
         screen.fill((0, 0, 0))
         pygame.display.flip()
         result = instructions(screen)
-        
 
-# parsed = False
-#   while not parsed:
-#       try:
-#           size_y = int(raw_input("NUMBER OF ROWS: "))
-#           parsed = True
-#       except ValueError:
-#           print "INVALID INPUT"
+if list_result[0] == 3: #start game
+    input_list = list_result[1]
+    num_players = input_list[0]
+    num_balls = input_list[1]
+    ball_speed = input_list[2] / 2
 
-# parsed = False
-#   while not parsed:
-#       try:
-#           size_x = int(raw_input("NUMBER OF COLUMNS: "))
-#           parsed = True
-#       except ValueError:
-#           print "INVALID INPUT"
-
-
-
-# if stop == True:
-
-
-        #     again = raw_input("Would you like to run the simulation again? If yes, type 'yes'\n")
-        #     if again == 'yes':
-        #         new_game()
-        # while stop == False:
-        #     for event in pygame.event.get():
-        #         if event.type == pygame.QUIT: #user clicks close
-        #             stop = True
-        #             pygame.quit()
-        #         elif event.type==pygame.KEYDOWN:
-        #             if event.key==pygame.K_p:
-        #                 if pause:
-        #                     pause = False
-        #                     print "GAME RESUMED"
-        #                 else:
-        #                     pause = True
-        #                     print "GAME PAUSED"
+    board = Board(num_players, num_balls, ball_speed)
+    clock = pygame.time.Clock()
+    game = Game(screen, board, clock)
+    game.main_loop(num_players, num_balls, ball_speed)
