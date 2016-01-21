7e72e3a8aae036b9b8f33750f0b0ede024bf55fe
cokumko <cokumko@mit.edu>
2016-01-21 16:59:12 -0500

    Update ball_class.py

--- ball_class.py
+++ ball_class.py
@@ -1,10 +1,13 @@
 '''
 Ball Class
 '''
-
+from paddle_class import Paddle
 from physical_object import PhysObj
 
 import pygame, random, math
+
+from wall_class import Wall
+
 
 class Ball(PhysObj):
     def __init__(self, color, vel, x, y):
@@ -48,45 +51,45 @@ class Ball(PhysObj):
 
     def changeDir(self, obj):
         if self.isCollis(obj):
-        	if type(obj) is Paddle or type(obj) is Wall:
-        		if obj.orientation % 2 == 0: # even number: top or bottom
-        			# bottom: 5*pi/4 --> 3*pi/4; 7*pi/4 --> pi/4
-        			# top: 3*pi/4 --> 5*pi/4; pi/4 --> 7*pi/4
-        			self.angle = 2 * math.pi - self.angle	
-        		elif obj.orientation % 2 == 1: # odd number: left or right
-        			# right: 7*pi/4 --> 5*pi/4; pi/4 --> 3*pi/4
-        			# left: 5*pi/4 --> 7*pi/4; 3*pi/4 --> pi/4
-        			if self.angle <= math.pi:
-        				self.angle = math.pi - self.angle
-        			elif self.angle > math.pi:
-        				self.angle = 3 * math.pi - self.angle
-        	elif type(obj) is Ball:
-        		# pi/4 --> 3*pi/4; 3*pi/4 --> pi/4; 5*pi/4 --> 7*pi/4; 7*pi/4 --> 5*pi/4
-        		if self.angle <= math.pi:
-        			self.angle = math.pi - self.angle
-       			elif self.angle > math.pi:
-       				self.angle = 3 * math.pi - self.angle
-	return self.get_angle()
+            if type(obj) is Paddle or type(obj) is Wall:
+                if obj.orientation % 2 == 0: # even number: top or bottom
+                    # bottom: 5*pi/4 --> 3*pi/4; 7*pi/4 --> pi/4
+                    # top: 3*pi/4 --> 5*pi/4; pi/4 --> 7*pi/4
+                    self.angle = 2 * math.pi - self.angle
+                elif obj.orientation % 2 == 1: # odd number: left or right
+                    # right: 7*pi/4 --> 5*pi/4; pi/4 --> 3*pi/4
+                    # left: 5*pi/4 --> 7*pi/4; 3*pi/4 --> pi/4
+                    if self.angle <= math.pi:
+                        self.angle = math.pi - self.angle
+                    elif self.angle > math.pi:
+                        self.angle = 3 * math.pi - self.angle
+            elif type(obj) is Ball:
+                # pi/4 --> 3*pi/4; 3*pi/4 --> pi/4; 5*pi/4 --> 7*pi/4; 7*pi/4 --> 5*pi/4
+                if self.angle <= math.pi:
+                    self.angle = math.pi - self.angle
+                elif self.angle > math.pi:
+                    self.angle = 3 * math.pi - self.angle
+        return self.get_angle()
         #returns new angle of ball
         #if position of ball is equal to pos range of paddles then return true
         #loop through each paddle and add/subtract length of paddle
         
         # checks if ball is off screen (used later in function below)
-        def isOffScreen(self):
-        	if 0 < self.pos_x and self.pos_x > 500 and 0 < self.pos_y and self.pos_y > 500:
-        		return False
-        	return True
+    def isOffScreen(self):
+        if 0 < self.pos_x and self.pos_x > 500 and 0 < self.pos_y and self.pos_y > 500:
+            return False
+        return True
         
-        # returns orientation of player who lost ball and 0 if ball is still on board
-        def offScreenOrientation(self):
-        	if self.isOffScreen():
-        		if self.pos_x > 500 and self.pos_y < 500:
-        			return 1
-        		elif self.pos_x > 0 and self.pos_y > 500:
-        			return 2
-        		elif self.pos_x < 0 and self.pos_y > 0:
-        			return 3
-        		elif self.pos_x < 500 and self.pos_y < 0:
-        			return 4
-        	else:
-        		return 0
+    # returns orientation of player who lost ball and 0 if ball is still on board
+    def offScreenOrientation(self):
+        if self.isOffScreen():
+            if self.pos_x > 500 and self.pos_y < 500:
+                return 1
+            elif self.pos_x > 0 and self.pos_y > 500:
+                return 2
+            elif self.pos_x < 0 and self.pos_y > 0:
+                return 3
+            elif self.pos_x < 500 and self.pos_y < 0:
+                return 4
+        else:
+            return 0
