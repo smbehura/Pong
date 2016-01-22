ffb281c8850dfb4d75442e4a7ef9936bf5aadc48
cokumko <cokumko@mit.edu>
2016-01-22 03:31:04 -0500

    Update player_class.py

--- player_class.py
+++ player_class.py
@@ -14,19 +14,44 @@ class Player(object):
 		self.state = 0
 
 	def get_paddle(self):
+		'''
+		returns paddle variable
+		'''
 		return self.paddle
 
 	def get_orient(self):
+		'''
+		returns orient variable
+		'''
+		
 		return self.orient
 
 	def get_state(self):
+		'''
+		returns state variable
+		'''
+		
 		return self.state
 
 	def neutral(self):
+		'''
+		sets state variable to 0
+		returns None
+		'''
+		
 		self.state = 0
 
 	def win(self):
+		'''
+		sets state variable to 1
+		returns None
+		'''
+		
 		self.state = 1
 
 	def lose(self):
+		'''
+		sets state variable to -1
+		returns None
+		'''
 		self.state = -1
