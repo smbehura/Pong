547db779730934f5d82f123a0e55f2e53692986c
smbehura <smbehura@gmail.com>
2016-01-22 03:22:03 -0500

    Update physical_object.py

--- physical_object.py
+++ physical_object.py
@@ -13,6 +13,9 @@ class PhysObj(pygame.sprite.Sprite):
     self.pos_y = y
     self.update_rect()
     
+  # isCollis checks if self has collided or has overlap with input object
+  # input: object which self is being checked against (for overlap)
+  # output: boolean depending on whether or not there is overlap/collision
   def isCollis(self, obj):
     x = obj.get_x
     y = obj.get_y
@@ -23,28 +26,39 @@ class PhysObj(pygame.sprite.Sprite):
     # return False
     return rect2.colliderect(rect)
   
+  # get_x returns the x position of self
+  # input: none
+  # output: integer value representing x position
   def get_x(self):
     return self.pos_x
 
+  # get_y returns the y position of self
+  # input: none
+  # output: integer value representing y position
   def get_y(self):
     return self.pos_y
 
+  # get_rect returns the rectangle representing self
+  # input: none
+  # output: rectangle object that represents self
   def get_rect(self):
     return self.rect
 
+  # update_rect updates rectangle object with current selfs' positions x and y
+  # input: none
+  # output: none but self.rect is updated to most recent changes in x and y positions
   def update_rect(self):
         self.rect.x = self.pos_x
         self.rect.y = self.pos_y
         
+  # move allows subclasses to override this function and provide their own method
+  # input: none
+  # output: none
   def move(self):
     pass
   
+  # changeDir allows subclasses to override this function and provide their own method
+  # input: none
+  # output: none
   def changeDir(self):
     pass
-        
-  def move():
-  	pass
-  
-  def changeDir():
-  	pass
-        
